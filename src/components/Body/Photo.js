49e483252401105fb077e49f429d4bb61e99f908
willwearing <willwearing@gmail.com>
2020-09-10 15:37:41 -0600

    updated with some animations on picture

--- src/components/Body/Photo.js
+++ src/components/Body/Photo.js
@@ -1,11 +1,21 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
-const kf = keyframes`
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
 
+  to {
+    transform: rotate(360deg);
+  }
 `;
 
-const StyledImg = styled.div``;
+const StyledImg = styled.div`
+  &:hover {
+    animation: ${rotate} 2s linear;
+  }
+`;
 
 export default function Photo(photo) {
   return (
